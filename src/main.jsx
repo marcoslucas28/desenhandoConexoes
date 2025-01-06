9e48e6a51f11bc23c462f56c63d9025d09d1607c
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2025-01-06 19:17:55 -0300

    create color palette

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+
 import './index.css'
 import App from './App.jsx'
 
 import GlobalStyles from './styles/global.js'
+import theme from './styles/theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export default {
+    COLORS: {
+        BLUE_100: "#88dcf9",
+        BLUE_200: "#3773ea",
+        BLUE_300: "#7c8cb9",
+        BLUE_400: "#041c6c",
+
+        GRAY: "55628a",
+
+        WHITE: "#ffffff"
+    }
+}
