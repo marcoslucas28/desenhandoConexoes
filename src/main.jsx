ff903366b85ed5718b48535ab03dfc0683f63d49
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2025-01-07 16:24:16 -0300

    create routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 
-import './index.css'
-import App from './App.jsx'
-
 import GlobalStyles from './styles/global.js'
 import theme from './styles/theme.js'
+
+import { Routes } from './routes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <App />
+      <Routes />
     </ThemeProvider>
   </StrictMode>,
 )
