fff936c15e04a41c895c9af3e2292f7dc35177f7
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2025-01-22 18:08:14 -0300

    create draw route

---
+++ src/pages/Draw/index.jsx
@@ -0,0 +1,7 @@
+import { Container } from './styles'
+
+export function Draw(){
+    return(
+        <Container>Página de desenho</Container>
+    )
+}
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import { Home } from '../pages/Home'
 import { About } from '../pages/About'
+import { Draw } from '../pages/Draw'
 
 export function AppRoutes(){
     return(
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/draw' element={<Draw />} />
             </Routes>
     )
 }
